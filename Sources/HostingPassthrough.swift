2164900f3a3abbe8cd36acd85d2af5c9097605f4
Alex Ehlke <alex.ehlke@gmail.com>
2025-04-03 02:32:14 -0400

    wip

--- Sources/HostingPassthrough.swift
+++ Sources/HostingPassthrough.swift
@@ -54,7 +54,7 @@ open class HostingParentView: UIView {
     private func checkBehind(view: UIView, point: CGPoint, event: UIEvent?) -> UIView? {
         // if the hittest lands on a hosting view, and it has user interaction enabled, we check behind it.
         // otherwise just return the view directly (this is almost definitely a UIKit view).
-        if let view = hostingViews.first(where: { $0 == view }), view.isUserInteractionEnabled {
+        if let view = hostingViews.first(where: { $0 == view }), view.isUserInteractionEnabled, !(event?.type == .scroll) {
             // in order to check behind the _UIHostingView that captures all of the touches, we can tell it to stop accepting touches, then perform another hittest in the same location to see what's underneath it.
             view.isUserInteractionEnabled = false
             guard let hitBehind = super.hitTest(point, with: event) else { return nil }
